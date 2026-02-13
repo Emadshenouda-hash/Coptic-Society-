8e32c5851b4ced81693dd6fdb1257b5f810edb8e
Emad <omarjoker1217@gmail.com>
2026-02-13 15:27:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2501373231-a63f6",
+  "appId": "1:868086002133:web:77c75ae83fb2ff2c0d8d5c",
+  "apiKey": "AIzaSyBrsyiDDfjU_scnLkw1bQZzNEcG5LXKSAE",
+  "authDomain": "studio-2501373231-a63f6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "868086002133"
+};
