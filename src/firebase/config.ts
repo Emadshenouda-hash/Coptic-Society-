d9d8bfd5eb8c26490820a66d150f364d7a84a1df
Emad <omarjoker1217@gmail.com>
2026-02-14 09:58:43 +0000

    I did everything you asked me to, and I still stuck

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-2501373231-a63f6",
   "appId": "1:868086002133:web:77c75ae83fb2ff2c0d8d5c",
-  "storageBucket": "copticsociety-media-storage-v1",
+  "storageBucket": "copticsociety-media-storage",
   "apiKey": "AIzaSyBrsyiDDfjU_scnLkw1bQZzNEcG5LXKSAE",
   "authDomain": "studio-2501373231-a63f6.firebaseapp.com",
   "measurementId": "",
