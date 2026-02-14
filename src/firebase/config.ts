c7a3774a05d3f40aa7d34ef49cce2ba1b8c29cee
Emad <omarjoker1217@gmail.com>
2026-02-14 09:36:47 +0000

    To create a bucket named studio-2501373231-a63f6.appspot.com, you must v

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-2501373231-a63f6",
   "appId": "1:868086002133:web:77c75ae83fb2ff2c0d8d5c",
-  "storageBucket": "studio-2501373231-a63f6.appspot.com",
+  "storageBucket": "YOUR_UNIQUE_BUCKET_NAME_HERE",
   "apiKey": "AIzaSyBrsyiDDfjU_scnLkw1bQZzNEcG5LXKSAE",
   "authDomain": "studio-2501373231-a63f6.firebaseapp.com",
   "measurementId": "",
